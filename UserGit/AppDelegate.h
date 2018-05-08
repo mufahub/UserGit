dd7521844533984b51b35ad89e56855d10b07862
ztman <m18256766725@163.com>
2018-05-08 09:06:33 +0800

    Initial Commit

---
+++ UserGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserGit
+//
+//  Created by ztman on 2018/5/8.
+//  Copyright © 2018年 LSXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UserGit
+//
+//  Created by ztman on 2018/5/8.
+//  Copyright © 2018年 LSXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
